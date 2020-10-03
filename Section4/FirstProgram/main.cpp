4ae9acf8a21ad905dd0eb4232c2bcd3bd47fa591
Alex Trzeciak <trebek08@gmail.com>
2020-10-03 14:53:47 -0700

    inital commikt

---
+++ Section4/FirstProgram/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(){
+    int favorite_number;
+    std::cout << "Enter Your Favorite Number between 1 and 100: ";
+    std::cin >> favorite_number;
+    std::cout << "Amazing!! That is my favorite number too!" << std::endl;
+    return 0;
+}
